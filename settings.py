df26b9d08c099bdb8002c9b71defa75459bc43ea
k1000 <selwak@gmail.com>
2011-08-27 10:33:13 +0100

    corrected preject url in settings

--- settings.py
+++ settings.py
@@ -102,7 +102,7 @@ MIDDLEWARE_CLASSES = (
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 )
 
-ROOT_URLCONF = 'stratusproject.urls'
+ROOT_URLCONF = 'stratus-project.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
